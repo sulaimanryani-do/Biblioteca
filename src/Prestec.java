7382c0371a55cf03e13bfe16c94c9ee39b70b1e9
Miquel <miquel.larios@insbaixcamp.cat>
2026-05-09 12:20:55 +0200

    Afegits comentaris JavaDoc a Prestec

--- src/Prestec.java
+++ src/Prestec.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
-// classe que representa un prestec d'un llibre a un usuari
+/**
+ * classe que representa un prestec d'un llibre a un usuari.
+ */
 public class Prestec {
 
     private Usuari usuari;
@@ -9,47 +11,71 @@ public class Prestec {
     private LocalDate dataRetorn;
     private boolean retornat;
 
-    // constructor que crea un nou prestec
+    /**
+     * constructor que crea un nou prestec.
+     * @param usuari L'usuari que fa el prestec
+     * @param llibre El llibre que es presta
+     * @param dataPrestec La data en que es fa el prestec
+     */
     public Prestec(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
         this.usuari = usuari;
         this.llibre = llibre;
         this.dataPrestec = dataPrestec;
-        // El llibre s'ha de retornar en 2 setmanes
         this.dataRetorn = dataPrestec.plusWeeks(2);
         this.retornat = false;
     }
 
-    // retorna l'usuari del prestec
+    /**
+     * retorna l'usuari del prestec.
+     * @return usuari
+     */
     public Usuari getUsuari() {
         return usuari;
     }
 
-    // retorna el llibre del prestec
+    /**
+     * retorna el llibre del prestec.
+     * @return llibre
+     */
     public Llibre getLlibre() {
         return llibre;
     }
 
-    // retorna la data en que es va fer el prestec
+    /**
+     * retorna la data en que es va fer el prestec.
+     * @return data del prestec
+     */
     public LocalDate getDataPrestec() {
         return dataPrestec;
     }
 
-    // retorna la data límit per tornar el llibre
+    /**
+     * retorna la data límit per tornar el llibre.
+     * @return data de retorn
+     */
     public LocalDate getDataRetorn() {
         return dataRetorn;
     }
 
-    // comprova si el llibre ja ha estat retornat
+    /**
+     * comprova si el llibre ja ha estat retornat.
+     * @return true si ha estat retornat, false si no
+     */
     public boolean esRetornat() {
         return retornat;
     }
 
-    // marca el prestec com a retornat
+    /**
+     * marca el prestec com a retornat.
+     */
     public void marcarRetornat() {
         retornat = true;
     }
 
-    // mostra la informació del prestec
+    /**
+     * mostra la informació del prestec.
+     * @return string amb la informació del prestec
+     */
     @Override
     public String toString() {
         return "Llibre: " + llibre.getTitol() +
